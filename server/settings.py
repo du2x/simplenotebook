b33802021ece037242172272035daf0e5c572619
du2x <dudumonteiro@gmail.com>
2017-08-19 18:22:31 -0300

    Compliance with @angular/cli

--- server/settings.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'iwillchooseabettersecretipromess'
-
-APP_NAME = 'simplenotebook'
-
-DEBUG = True
-TESTING = False
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://simplenotebook:mypassword@postgres:5432/simplenotebook'
