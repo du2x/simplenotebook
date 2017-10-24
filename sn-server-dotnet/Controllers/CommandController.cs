7686b29a70c7b431ca2fc4079934cb2c9318207b
Eduardo Rocha Monteiro <eduardo.monteiro@serpro.gov.br>
2017-10-24 13:18:12 -0200

    General improvements

--- sn-server-dotnet/Controllers/CommandController.cs
+++ sn-server-dotnet/Controllers/CommandController.cs
@@ -11,6 +11,7 @@ using Npgsql;
 using System.Data;
 using Npgsql.Schema;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace sn_server_dotnet.Controllers
 {
@@ -23,23 +24,21 @@ namespace sn_server_dotnet.Controllers
         {
             NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=simplenotebook; " + 
             "Password=mypassword;Database=simplenotebook;");            
-            conn.Open();            
-            NpgsqlCommand cmd = new NpgsqlCommand((string)sql.GetValue("sql"), conn);
-            NpgsqlDataReader dr = cmd.ExecuteReader();       
-            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-            Dictionary<string, object> row;
-            while (dr.Read()) {
-                row = new Dictionary<string, object>();
-                foreach (NpgsqlDbColumn col in dr.GetColumnSchema())
-                {
-                    row.Add(col.ColumnName, dr.GetFieldValue<Object>((int)col.ColumnOrdinal) );
-                }
-                rows.Add(row);
-            }           
-            dr.Close();
-            conn.Close();          
-            string res = JsonConvert.SerializeObject(rows);
-            return  res;
+            conn.Open();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter((string)sql.GetValue("sql"), conn);
+            DataSet ds = new DataSet();            
+            da.Fill(ds);
+            conn.Close();
+            string result = JsonConvert.SerializeObject(ToDictionary(ds.Tables[0]), Formatting.Indented).Replace("\"", "");
+            return result;
         }
+
+        public static IEnumerable<Dictionary<string, object>> ToDictionary(DataTable table)
+        {
+            string[] columns = table.Columns.Cast<DataColumn>().Select(c=>c.ColumnName).ToArray();
+            IEnumerable<Dictionary<string, object>>  result = table.Rows.Cast<DataRow>()
+                    .Select(dr => columns.ToDictionary(c => c, c=> dr[c]));
+            return result;
+        }        
     }
 }
